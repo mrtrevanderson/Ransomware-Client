784d1554aee62e07a41db73ded2806429ae9b577
Trevor Anderson <trevoranderson1@hotmail.com>
2018-12-11 13:38:26 -0800

    Added Constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+
+AES_KEYSIZE = 32  # AES key size, in bits
+AES_IVSIZE = 16  # AES IV size, in bits
+
+RSA_PUBLIC_EXPONENT = 65537
+RSA_KEYSIZE = 2048  # RSA key size, in bits
+
+
+RSA_PUBLICKEY_FILEPATH = "./rsa_public.pem"
+RSA_PRIVATEKEY_FILEPATH = "./rsa_private.pem"
+
+ROOT_FOLDER = "./dangerzone"
+
+
+ENCRYPTED_EXTENSION = ".tradlock"
+
+NETWORK_HOST = "https://tradlab.me"
+NETWORK_ROUTE = "/tasks"
